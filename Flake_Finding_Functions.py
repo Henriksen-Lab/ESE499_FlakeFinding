38db4806ccd736e799ec45d65e6549435d16c639
kaitlinday <54457484+kaitlinday@users.noreply.github.com>
2022-11-21 11:12:51 -0600

    functions

---
+++ Flake_Finding_Functions.py
@@ -0,0 +1,29 @@
+def find_index(lst, item):
+    return [i for i, x in enumerate(lst) if x == item]
+
+def region(flake_loc_x, flake_loc_y):
+    flake_regions = []
+    for i in range(len(flake_loc_x)):
+        if flake_loc_x[i] <= 256:
+            if flake_loc_y[i] <= 341:
+                flake_regions.append("UL")
+            elif flake_loc_y[i] <= 682:
+                flake_regions.append("UM")
+            else:
+                flake_regions.append("UR")
+        elif flake_loc_x[i] <= 512:
+            if flake_loc_y[i] <= 341:
+                flake_regions.append("ML")
+            elif flake_loc_y[i] <= 682:
+                flake_regions.append("MM")
+            else:
+                flake_regions.append("MR")
+        else:
+            if flake_loc_y[i] <= 341:
+                flake_regions[i].append("BL")
+            elif flake_loc_y[i] <= 682:
+                flake_regions.append("BM")
+            else:
+                flake_regions.append("BR")
+    
+    return flake_regions
