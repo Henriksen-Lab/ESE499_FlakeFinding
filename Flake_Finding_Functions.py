8bacf81ee35e8bb1aec1efe2f10526ce291780ce
kaitlinday <54457484+kaitlinday@users.noreply.github.com>
2022-11-21 17:28:35 -0600

    update functions

--- Flake_Finding_Functions.py
+++ Flake_Finding_Functions.py
@@ -3,28 +3,27 @@ def find_index(lst, item):
 
 def region(flake_loc_x, flake_loc_y):
     flake_regions = []
-    for i in range(len(flake_loc_x)):
-        if flake_loc_x[i] <= 256:
-            if flake_loc_y[i] <= 341:
-                flake_regions.append("UL")
-            elif flake_loc_y[i] <= 682:
-                flake_regions.append("UM")
-            else:
-                flake_regions.append("UR")
-        elif flake_loc_x[i] <= 512:
-            if flake_loc_y[i] <= 341:
-                flake_regions.append("ML")
-            elif flake_loc_y[i] <= 682:
-                flake_regions.append("MM")
-            else:
-                flake_regions.append("MR")
+    if flake_loc_x <= 256:
+        if flake_loc_y <= 341:
+            flake_regions.append("UL")
+        elif flake_loc_y <= 682:
+            flake_regions.append("UM")
         else:
-            if flake_loc_y[i] <= 341:
-                flake_regions[i].append("BL")
-            elif flake_loc_y[i] <= 682:
-                flake_regions.append("BM")
-            else:
-                flake_regions.append("BR")
+            flake_regions.append("UR")
+    elif flake_loc_x <= 512:
+        if flake_loc_y <= 341:
+            flake_regions.append("ML")
+        elif flake_loc_y <= 682:
+            flake_regions.append("MM")
+        else:
+            flake_regions.append("MR")
+    else:
+        if flake_loc_y <= 341:
+            flake_regions.append("BL")
+        elif flake_loc_y <= 682:
+            flake_regions.append("BM")
+        else:
+            flake_regions.append("BR")
     
     return flake_regions
 
